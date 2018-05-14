5346a1182b78f00c02d977279a168edba37699e6
Brad Roodt <brad.roodt@gmail.com>
2018-05-14 12:39:51 +0200

    feat: added form disable and loading icon display on form send for user feedback.

--- src/views/Contribute.vue
+++ src/views/Contribute.vue
@@ -5,12 +5,13 @@
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
-              <v-toolbar dark color="primary">
+              <v-toolbar dark flat color="primary">
                 <v-toolbar-title>Contribute to fund</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form ref="form" v-model="valid" lazy-validation>
                   <v-text-field
+                      :disabled="isSending"
                       prepend-icon="person"
                       v-model="amount"
                       :rules="amountRules"
@@ -18,6 +19,7 @@
                       required>
                   </v-text-field>
                   <v-select
+                      :disabled="isSending"
                       prepend-icon="person"
                       v-model="unit"
                       :rules="unitRules"
@@ -30,9 +32,11 @@
 
               <v-card-actions>
                 <v-spacer></v-spacer>
+                <p class="teal--text">{{formMsg}}</p>
                 <v-btn
                   primary
                   :disabled="!valid"
+                  :loading="isSending"
                   @click="submit">
                 Send
                 </v-btn>
@@ -50,6 +54,14 @@ import contractInstance from '../../ethereum/contractInstance';
 
 export default {
 	data: () => ({
+		formMsgStates: {
+			default: '',
+			sending: 'Sending transaction. This will take a minute',
+			failedTransaction: 'Something went wrong while sending your transaction',
+			success: 'Transaction was successful'
+		},
+		formMsg: '',
+		isSending: false,
 		valid: false,
 		amount: '',
 		amountRules: [
@@ -66,12 +78,14 @@ export default {
 	methods: {
 		async submit() {
 			if (this.$refs.form.validate()) {
+				this.isSending = true;
 				const accounts = await web3.eth.getAccounts();
 				const from = accounts[0];
 				const value = web3.utils.toWei(this.amount, this.unit.toLowerCase());
 				const tx = await contractInstance.methods
 					.contribute()
 					.send({ from, value });
+				this.isSending = false;
 				console.log(tx);
 			}
 		},
